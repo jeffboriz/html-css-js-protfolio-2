95a0b50a65bd2979aeecbb08384d1ce491ef699a
jeffboriz <99708349+jeffboriz@users.noreply.github.com>
2025-07-26 22:19:21 +0530

    Revert " "finished""
    This reverts commit 37011674af14e9a37a88fdbfe1f530f395f6e3b0.

--- script.js
+++
@@ -1,7 +0,0 @@
-function toggleMenu(){
-    const menu =document.querySelector('.menu-links');
-    const icone = document.querySelector(".hamburger-icon");
-    menu.classList.toggle("open");
-    icone.classList.toggle("open");
-    
-}
